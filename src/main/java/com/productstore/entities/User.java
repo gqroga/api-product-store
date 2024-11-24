31dcb87c0319231ceb8d10d78e167b6c99b3f61f
gqroga <gcqueirogat@gmail.com>
2024-11-24 13:27:43 -0300

    feat: User entity, repository, service, resource and test config

---
+++ src/main/java/com/productstore/entities/User.java
@@ -0,0 +1,35 @@
+package com.productstore.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tb_user")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String name;
+    public String email;
+    public  String phone;
+    public  String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, String password) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+}
