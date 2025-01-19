315d14cac28c451ad7673d947bd87b47cb95297a
gqroga <gcqueirogat@gmail.com>
2025-01-19 11:04:55 -0300

    feat: Payment entity and one to one association

---
+++ src/main/java/com/productstore/entities/Payment.java
@@ -0,0 +1,36 @@
+package com.productstore.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @JsonIgnore
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Instant moment, Order order) {
+        this.id = id;
+        this.moment = moment;
+        this.order = order;
+    }
+}
